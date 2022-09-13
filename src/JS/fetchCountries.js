06cbe0a39a3724356943bd460052b002f3692af8
VitaliyGO <shuricks007@gmail.com>
2022-09-14 02:17:17 +0300

    create base script

---
+++ src/JS/fetchCountries.js
@@ -0,0 +1,16 @@
+'use strict'
+
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v2/name/${name.trim()}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+}
+
+
+
+
+    // https://restcountries.com/v3.1/name/${name.trim()}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
+import { fetchCountries } from './JS/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const countryDesc = document.querySelector('.country-info')
+
+console.log(input);
+
+input.addEventListener('input', debounce((event) => {
+    fetchCountries(event.target.value)
+        .then(countries => {
+            if(event.target.value === ''){
+                return countryDesc.innerHTML = '';
+            };
+            return countriesMarkup(countries)})
+        .catch(error => console.log(error));
+}, 300));
+
+function countriesMarkup(countries) {
+    console.log(countries);
+    const markup = countries.map(country => {
+        return `
+        <img src="${country.flags.svg}" alt="flag" width="50">
+        <h1 class="title">${country.name}</h1>
+        <p><b>Capital:</b> ${country.capital}</p>
+        <p><b>Population:</b> ${country.population}</p>
+        <p><b>Languages:</b> ${country.languages[0].name}</p>`;
+    }
+    ).join('');
+    countryDesc.innerHTML = markup;
+}
