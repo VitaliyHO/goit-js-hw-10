3f95ee66cc70be37f525019d47d98fef9703781f
VitaliyHO <100048629+VitaliyHO@users.noreply.github.com>
2022-09-13 19:59:09 +0000

    Deploying to gh-pages from @ VitaliyHO/goit-js-hw-10@68a452ea8f2b1d154d4f9a66a5682c627833dc9f 🚀

---
+++ 01-color-switcher.d290e952.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((function(){t.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3),t.startBtn.setAttribute("disabled","")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.d290e952.js.map
